5b9ded4d2e52f69e0cb8c7c18ce7f05883ae9261
Nikita <nikitatoshchev27@gmail.com>
2021-10-03 00:35:06 +0300

    Сделал компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const PLACE_COUNT = 312;
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placeCount={PLACE_COUNT} />
   </React.StrictMode>,
   document.getElementById('root'));
