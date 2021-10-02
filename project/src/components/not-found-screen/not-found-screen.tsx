e45f66361fa49ed0654d67dd73ce1019f1b57fb7
Nikita <nikitatoshchev27@gmail.com>
2021-10-03 00:48:51 +0300

    Добавил компонент несуществующей страницы

---
+++ project/src/components/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,10 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <div className="page page--gray">
+      <h1>404. Page not found</h1>
+      <a href="main.html">Back to the main page</a>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
