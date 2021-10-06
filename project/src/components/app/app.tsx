4cb88a842458b73d484e75f7f37dd9a6046de13e
Nikita <nikitatoshchev27@gmail.com>
2021-10-06 20:37:59 +0300

    Добавил роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+
 import MainScreen from '../main-screen/main-screen';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
+import FavotiresScreen from '../favorites-screen/favorites-screen';
+import RoomScreen from '../room-screen/room-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppProps = {
   placeCount: number;
@@ -6,8 +14,31 @@ type AppProps = {
 
 function App({ placeCount }: AppProps): JSX.Element {
   return (
-    <MainScreen placeCount={placeCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Main}>
+          <MainScreen placeCount={placeCount} />
+        </Route>
+        <Route exact path={AppRoute.SignIn}>
+          <SignInScreen />
+        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Favorites}
+          render={() => <FavotiresScreen />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
+        <Route exact path={AppRoute.Room}>
+          <RoomScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
--- project/src/components/not-found-screen/not-found-screen.tsx
+++ project/src/components/not-found-screen/not-found-screen.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom';
+
+
 function NotFoundScreen(): JSX.Element {
   return (
-    <div className="page page--gray">
+    <div className="page page--gray" style={{ margin: '100px 550px' }}>
       <h1>404. Page not found</h1>
-      <a href="main.html">Back to the main page</a>
+      <Link to="/">Back to the main page</Link>
     </div>
   );
 }
