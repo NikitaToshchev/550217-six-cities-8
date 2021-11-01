c6668ff84b59cf8be4d7d19cd14cd1db388ba141
Nikita <nikitatoshchev27@gmail.com>
2021-11-01 18:14:05 +0300

    Добавил токен

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { getToken } from './token';
 
 const BACKEND_URL = 'https://8.react.pages.academy/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -14,6 +15,18 @@ export const createAPI = (onUnauthorized: UnauthorizedCallback): AxiosInstance =
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
 
   api.interceptors.response.use(
     (response: AxiosResponse) => response,
