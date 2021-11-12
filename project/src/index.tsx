97cd3a908c59aa227dbc88826c696b8d3b9092c7
Nikita <nikitatoshchev27@gmail.com>
2021-11-12 21:12:16 +0300

    Изменил создание store по RTK

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { reducer } from './store/reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { AuthorizationStatus } from './const';
 import { createAPI } from './services/api';
-import thunk from 'redux-thunk';
 import { requireAuthorizationSucces } from './store/action';
 import { fetchOffersAction, checkAuthAction } from './store/api-actions';
 import { ThunkAppDispatch } from './types/actions';
+import { configureStore } from '@reduxjs/toolkit';
 
 const api = createAPI(
   () => store.dispatch(requireAuthorizationSucces(AuthorizationStatus.NoAuth)),
 );
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(
-    applyMiddleware(thunk.withExtraArgument(api))),
-);
+const store = configureStore({
+  reducer: reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
 
 (store.dispatch as ThunkAppDispatch)(checkAuthAction());
 (store.dispatch as ThunkAppDispatch)(fetchOffersAction());
